dd17d6a4e9234e05a99c6ed21de3eb00d3fcfcbf
Error200503 <147841330+Error200503@users.noreply.github.com>
2023-10-14 00:49:16 +0530

    Update Errorphish.sh

--- Errorphish.sh
+++ Errorphish.sh
@@ -213,7 +213,7 @@ banner() {
 		${ORANGE}      | |                                
 		${ORANGE}      |_|                ${RED}Version : ${__version__}
 
-		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by htr-tech (tahmid.rayat)${WHITE}
+		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by error.2005.03 (DARK.Q)${WHITE}
 	EOF
 }
 
